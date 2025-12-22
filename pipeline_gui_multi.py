798417276c99d5fdef75dd0cd7487da9901df65f
AriqHibatullah <ariqhibatullah03@gmail.com>
2025-12-23 00:48:52 +0700

    Update pipeline_gui_multi.py

--- pipeline_gui_multi.py
+++ pipeline_gui_multi.py
@@ -677,6 +677,8 @@ def home_page():
         with m3:
             st.markdown(f"""<div class="stat-card"><div class="stat-number">{len(df['Menu'].unique())}</div><div class="stat-label">Topik Bahasan</div></div>""", unsafe_allow_html=True)
 
+        st.markdown("<br>", unsafe_allow_html=True)
+        
     if df.empty:
         st.info("📂 Database masih kosong. Silakan login ke Admin untuk mengisi data.")
         return
